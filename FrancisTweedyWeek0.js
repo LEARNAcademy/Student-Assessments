081c1ed6aa5a4168b05bc0fda01b83a5a2b4d514
learn <learn@learn03.local>
2019-02-15 14:50:16 -0800

    added a file

--- FrancisTweedyWeek0.js
+++ FrancisTweedyWeek0.js
@@ -5,11 +5,12 @@ var mantra = "Be the dev";
 // 1. Write the code that returns 'B' from mantra.
 function getB(str){
     for (let i = 0; i < str.length; i++){
-        if (str.charAt[i] === 'B'){
-            return str[i];
+        if (str.charAt(i) === 'B'){
+            return str.charAt(i);
         }
     }
 }
+console.log(getB(mantra));
 
 // 2. Write the code that determines if there is a 'x' in mantra.
 function findX(str){
@@ -19,6 +20,7 @@ function findX(str){
         return 'String does not contain x';
     }
 }
+console.log(findX(mantra));
 
 // 3. Write the code that determines if there is a 'v' in mantra.
 function findV(str){
@@ -28,15 +30,17 @@ function findV(str){
         return 'String does not contain v';
     }
 }
+console.log(findV(mantra));
 
 // Stretch: Write the code that returns the position of 'v' in mantra.
-function findV(str){
+function posV(str){
     if (str.includes('v')){
         return str.indexOf('v');
     } else {
         return 'String does not contain v';
     }
 }
+console.log(posV(mantra));
 
 // Consider the following statement:
 
@@ -66,27 +70,43 @@ if (imLost || imFrustrated) {
 
 // 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
 for (let i = 0; i < mantra.length; i++){
-    console.log(mantra.charAt[i])
+    console.log(mantra.charAt(i));
 }
 
 // Stretch: Using a while loop.
 let whileCounter = 0;
 while (whileCounter < mantra.length){
-    console.log(mantra.charAt[whileCounter]);
+    console.log(mantra.charAt(whileCounter));
+    whileCounter++;
 }
 
-// Super Stretch: Using forEach().
-forEach (var item in mantra){
-    console.log(mantra.charAt[item]);
-}
+//Super Stretch: Using forEach().
+var mantraArr = mantra.split("");
+console.log(mantraArr)
+mantraArr.forEach(function(s){
+    console.log(s);
+});
 
 // Consider the following variable:
 
 var message = "thisisateststring"
 
 // 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+function noVowels(str){
+    return str.replace(/[aeiou]/gi, '');
+}
+console.log(noVowels(message));
 
 // Stretch: Update your function to throw an exception if the argument is not a string.
+var check = 2;
+function noVowelsStretch(str){
+    if (typeof(str) === "string"){
+        return str.replace(/[aeiou]/gi, '');
+    } else {
+        throw TypeError("Argument is not a string");
+    }
+}
+//console.log(noVowelsStretch(check));
 
 // Super Duper Stretch: Update your function to throw a TypeError if the argument is not a string.
 
@@ -95,3 +115,13 @@ var message = "thisisateststring"
 var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
 
 // 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
+function cats(arr){
+    var result = [];
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i].animal === 'cat'){
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+console.log(cats(toonimals));
