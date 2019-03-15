06b863966c4a40795c7da706a4d3e49cbdf69b7b
damonlemmons <jumproper22@gmail.com>
2019-03-14 17:50:56 -0700

    added rb file for week 5

---
+++ Week-5.rb
@@ -0,0 +1,27 @@
+# We've done the following exercises in Javascript, Now try them in RUBY (You can do them in Javascript again if you want extra practice - not required.)
+#
+# The World Translator
+# Write a function named helloWorld that:
+#
+# takes 1 argument, a language code (e.g. "es", "de", "en")
+# returns "Hello, World" for the given language, for at least 3 languages. It should default to returning English.
+# Test the function for each of the supported languages
+
+
+# The Grade Assigner
+# Write a function named assignGrade that:
+#
+# takes 1 argument, a number score.
+# returns a grade for the score, either "A", "B", "C", "D", or "F".
+# Test the function for a few different scores.
+
+
+# The Pluralizer
+# Write a function named pluralize that:
+#
+# takes 2 arguments, a noun and a number.
+# returns the number and pluralized form of the noun, if necessary.
+# pluralizer(5, cat) should return "5 cats"
+# pluralizer(1, dog) should return "1 dog" because the number one does not require a plural noun
+#
+# Bonus: Make it handle a few collective nouns like "sheep", "geese", "children", "people" and "species".
